7bc987711cdf2e2208c5ce960ed0d696c6150e17
Thayana Mamoré <thayanacmamore@gmail.com>
2020-10-05 16:01:42 -0300

    Second test of Heroku deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // Serve static files
-app.use(express.static(__dirname + '/public/dist'));
+app.use(express.static(__dirname + '/public'));
 
 // Serve your app
 console.log('Served: http://localhost:' + port);
